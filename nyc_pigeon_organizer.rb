d6ae85a6c53c8b1b28b8c1bd4d6d9036c00593fd
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-01 18:23:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |keys, values|
+      values.each do |item|
+          pigeon_list[item] = {} unless pigeon_list[item]
+          if !pigeon_list[item][key]
+          pigeon_list[item][key] = [keys.to_s] 
+          else 
+          pigeon_list[item][key].push(keys.to_s)
+          end
+      end
+    end
+  end
+  pigeon_list
 end
